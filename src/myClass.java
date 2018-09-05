85a4e8f99b8ec0330cd8871561d22553b04d7abe
Jonathan Roberge <sJonathanRoberge@yccc.edu>
2018-09-05 19:48:12 -0400

    Some CHanges

--- src/myClass.java
+++ src/myClass.java
@@ -3,6 +3,12 @@ public class myClass {
 
 	public static void main(String[] args) {
 		
+		private String name = "Jon";
+		private String lastName = "Roberge";
+		
+		
+		
+		
 		System.out.println("Welcome to my program");
 	
 	}
